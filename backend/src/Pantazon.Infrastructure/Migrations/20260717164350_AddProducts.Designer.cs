286c14ca45cffa77facc58434a76094b0b6d6304
Lázaro <lazaro.19251953@gmail.com>
2026-07-17 13:56:55 -0300

    update migrations

--- backend/src/Pantazon.Infrastructure/Migrations/20260717164350_AddProducts.Designer.cs
+++ backend/src/Pantazon.Infrastructure/Migrations/20260717164350_AddProducts.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Pantazon.Infrastructure.Data;
@@ -11,9 +12,11 @@ using Pantazon.Infrastructure.Data;
 namespace Pantazon.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260717164350_AddProducts")]
+    partial class AddProducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
