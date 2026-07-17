df1899d9594ff6030517f99eed17b050e3705efc
Lázaro <lazaro.19251953@gmail.com>
2026-07-17 13:50:07 -0300

    feat(api-service) add interfaces for services

---
+++ backend/src/Pantazon.Application/Services/IService/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using Pantazon.Domain.Entities;
+
+namespace Pantazon.Application.Services.IService
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>?> GetAll();
+        Task<Product?> GetByName(Action<Product> action);
+        Task Create();
+        Task Update(Product product);
+        Task Delete(Guid guid);
+    }
+}
