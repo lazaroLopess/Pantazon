5f849cb84946e4aa757b8ee92d26a7a047959605
Lázaro <lazaro.19251953@gmail.com>
2026-07-17 12:14:23 -0300

    feat(infrastructure) - add applicationDbContext

---
+++ backend/src/Pantazon.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pantazon.Domain.Entities;
+
+namespace Pantazon.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
