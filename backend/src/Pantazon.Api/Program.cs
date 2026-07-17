6fac502fffa77264f6718366ff2a190c5856ad93
Lázaro <lazaro.19251953@gmail.com>
2026-07-17 13:59:02 -0300

    add swagger for support api endpoints and popule db with some data

--- backend/src/Pantazon.Api/Program.cs
+++ backend/src/Pantazon.Api/Program.cs
@@ -1,3 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+using Pantazon.Application.Services;
+using Pantazon.Application.Services.IService;
+using Pantazon.Infrastructure;
+using Pantazon.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,13 +13,38 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new NullReferenceException("invalid connection string");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(connectionString, x => x.MigrationsAssembly("Pantazon.Infrastructure"));
+});
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
+builder.Services.AddScoped<IProductService, ProductService>();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ApplicationDbContext>();
+
+    await context.Database.MigrateAsync();
+    await DatabaseSeeder.SeedCategoriesAsync(context);
+    await DatabaseSeeder.SeedProductsAsync(context);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI();
+    
 }
+app.UseSwagger();
 
 app.UseHttpsRedirection();
 
